da53ecd72d3542dbb52a796d8de268f610aa0521
Anna Shevchenko <uaskoa@gmail.com>
2021-04-15 22:30:05 +0300

    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Profile from "./components/Profile"
 import Statistics from "./components/Statistics"
 import FriendList from "./components/FriendList"
+import TransactionHistory from "./components/TransactionHistory"
 import user from "./user.json"
 import statisticalData from "./statistical-data.json"
 import friends from "./friends.json"
+import transactions from "./transactions.json"
 
+console.log(transactions);
 
 const App = () => {
   return (
@@ -17,8 +20,9 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-          <Statistics title="Upload stats" stats={statisticalData} />
-          <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
